1c76a092c3e513d90f613e5c831646f9c135a68e
Nathan Taylor <taylnath@oregonstate.edu>
2021-05-25 12:16:46 -0700

    debugging sessions 2 -- took out mkdtemp()

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, session, redirect, request
-# from flask_session import Session
+from flask_session import Session
 from tempfile import mkdtemp
 from JanggiGame import JanggiGame as game
 import time
@@ -8,11 +8,11 @@ import os
 
 app = Flask(__name__, static_folder="./client/build", static_url_path="/")
 
-# # Configure session to use filesystem (instead of signed cookies)
+# Configure session to use filesystem (instead of signed cookies)
 # app.config["SESSION_FILE_DIR"] = mkdtemp()
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 def eprint(*msg):
     print(*msg, file=sys.stderr)
